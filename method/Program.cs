9a1adf73701062768494578f96b9f076d7edfdc2
Yasin <107244487+masora2@users.noreply.github.com>
2022-07-31 11:16:31 +0300

    methot3

---
+++ method/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace methoddeneme
+{
+    class method
+    {
+        static void Main(string[] args)
+        {
+            MainClass mainClass = new MainClass();
+
+            mainClass.DenemeMethod();
+            Console.ReadLine();
+        }
+    }
+    public class MainClass
+{
+    public void VoidMethodlarOrnek()
+    {
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+            Console.WriteLine("VoidMethodlarOrnek Çalıştı");
+        }
+
+    public void DenemeMethod()
+    {
+        Console.WriteLine("DenemeMethod Çalıştı");
+
+        VoidMethodlarOrnek();
+        Console.WriteLine("Program Bitti");
+    }
+
+
+}
+
+}
